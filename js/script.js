461a131b29834beb3011aa1413a56f0cd2b14ede
Debawi Maria <32854893+mariadebawi@users.noreply.github.com>
2019-01-20 12:01:30 +0100

    08

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 /*================================
-    | | | Preloader 
+    | | | Preloader | | |
 ==================================*/
 $(window).on('load', function () {
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
+});
+
+/*================================
+    | | | Team owl-carousel | | |
+==================================*/
+$(document).ready(function(){
+    $('#team-members').owlCarousel({
+        items:2 ,
+        autoplay:true ,
+        SmartSpeed : 700 ,
+        autoplayHoverPause: true,
+        nav: true, /* <> */
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1
+            },
+            // breakpoint from 480 up
+            480: {
+                items: 2
+            }
+        }
+  });
 });
