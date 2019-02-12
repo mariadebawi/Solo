d255dc6677cca0181a8da0a170f471e1b63f3381
Debawi Maria <32854893+mariadebawi@users.noreply.github.com>
2019-02-12 13:06:55 +0100

    17

--- js/script.js
+++ js/script.js
@@ -9,14 +9,15 @@ $(window).on('load', function () {
 /*================================
     | | | Team owl-carousel | | |
 ==================================*/
-$(document).ready(function(){
+$(document).ready(function () {
     $('#team-members').owlCarousel({
-        items:2 ,
-        loop : true ,
-        autoplay:true ,
-        SmartSpeed : 700 ,
+        items: 2,
+        loop: true,
+        autoplay: true,
+        SmartSpeed: 700,
         autoplayHoverPause: true,
-        nav: true, /* <> */
+        nav: true,
+        /* <> */
         dots: false,
         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
         responsive: {
@@ -29,31 +30,31 @@ $(document).ready(function(){
                 items: 2
             }
         }
-  });
+    });
 });
 
 /*================================
     | | | Progress Bars | | |
 ==================================*/
-$(function(){
-    $("#progress-elements").waypoint(function(){
-       // alert("hiiiii") ;
-       $(".progress-bar").each(function(){
-        $(this).animate({
-          width: $(this).attr("aria-valuenow") + "%"
-       } ,1000) ;
-      });
-        this.destroy() ;
-    } , {
-        offset : "bottom-in-view"
+$(function () {
+    $("#progress-elements").waypoint(function () {
+        // alert("hiiiii") ;
+        $(".progress-bar").each(function () {
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 1000);
+        });
+        this.destroy();
+    }, {
+        offset: "bottom-in-view"
     });
-   
+
 });
 
 /*================================
     | | | Responsive Tabs | | |
 ==================================*/
-$(function(){
+$(function () {
     $('#services-tabs').responsiveTabs({
         animation: 'slide', // The panels will slide up and down
     });
@@ -63,22 +64,35 @@ $(function(){
     | | | Isotop Filters | | |
 ==================================*/
 $(window).on('load', function () {
-   //Initialize Isotope
-   $('#isotop-container').isotope({
-  });
-  //filter items on click button
-  $('#isotop-filters').on('click' , 'button' , function(){
-     //alert("you click me") ;
-     //get the value name of filter
-     var filterValue = $(this).attr('data-filter') ;
-       //alert('you click ' + filterValue) ;
-       //filter portfolio 
-       $('#isotop-container').isotope({
-           filter : filterValue
-       });
-       //active button
-       $('#isotop-filters').find('.active').removeClass('active') ;
-       $(this).addClass('active') ;
+    //Initialize Isotope
+    $('#isotop-container').isotope({});
+    //filter items on click button
+    $('#isotop-filters').on('click', 'button', function () {
+        //alert("you click me") ;
+        //get the value name of filter
+        var filterValue = $(this).attr('data-filter');
+        //alert('you click ' + filterValue) ;
+        //filter portfolio 
+        $('#isotop-container').isotope({
+            filter: filterValue
+        });
+        //active button
+        $('#isotop-filters').find('.active').removeClass('active');
+        $(this).addClass('active');
 
-  });
+    });
+});
+
+/*================================
+    | | | MAgnific Popup | | |
+==================================*/
+$(function () {
+    /** Initialize  */
+    $('#portfolio-wrapper').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true // displaay all galery 
+        },
+    });
 });
